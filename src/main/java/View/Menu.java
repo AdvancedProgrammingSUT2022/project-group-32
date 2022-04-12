5a76607657edf29f986b662110675b577e6b4236
parsa <mpmb81@gmail.com>
2022-04-13 01:23:27 +0430

    add project base structure/.gitignore

---
+++ src/main/java/View/Menu.java
@@ -0,0 +1,14 @@
+package View;
+
+public class Menu {
+    public enum MenuType{
+        MAIN_MENU,
+        LOGIN_MENU,
+        GAME,
+        PROFILE,
+        EXIT;
+    }
+    public static run(){
+        return
+    }
+}
