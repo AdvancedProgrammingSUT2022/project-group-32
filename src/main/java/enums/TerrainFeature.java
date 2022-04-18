c7ae8f2cfd7aaac2b3d059bdd5e80618a5c24446
parsa <mpmb81@gmail.com>
2022-04-18 21:21:10 +0430

    TerrainType and TerrainFeature enums

--- src/main/java/enums/TerrainFeature.java
+++ src/main/java/enums/TerrainFeature.java
@@ -1,8 +1,28 @@
 package enums;
 
 public enum TerrainFeature {
-    // TerrainFeatures AND TerrainTypes
-    DESERT
+    // TerrainFeatures
+    FLOOD_PLAINS("Flood Plains"),
+    FOREST("Forest"),
+    ICE("Ice"),
+    JUNGLE("Jungle"),
+    MARSH("Marsh"),
+    OASIS("Oasis"),
+    RIVERS("Rivers"),
 
+    // base terrains
+    COAST("Coast"),
+    DESERT("Desert"),
+    GRASSLAND("Grassland"),
+    HILL("Hill"),
+    MOUNTAIN("Mountain"),
+    OCEAN("Ocean"),
+    PLAINS("Plains"),
+    SNOW("Snow");
 
+    private final String name;
+
+    TerrainFeature(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/enums/TerrainType.java
+++ src/main/java/enums/TerrainType.java
@@ -1,12 +1,29 @@
 package enums;
 
 public enum TerrainType {
-    Dessert(TerrainFeature.DESERT);
+    COAST(TerrainFeature.COAST),
+    DESERT(TerrainFeature.DESERT),
+    GRASSLAND(TerrainFeature.GRASSLAND),
+    HILL(TerrainFeature.HILL),
+    MOUNTAIN(TerrainFeature.MOUNTAIN),
+    OCEAN(TerrainFeature.OCEAN),
+    PLAINS(TerrainFeature.PLAINS),
+    SNOW(TerrainFeature.SNOW);
 
 
     private final TerrainFeature terrainFeature;
 
     TerrainType(TerrainFeature terrainFeature) {
         this.terrainFeature = terrainFeature;
+    }
+
+    public static TerrainFeature getTerrainFeatureByTerrainType(TerrainType terrainType) {
+        return terrainType.terrainFeature;
+    }
+
+    public static TerrainType getTerrainTypeByTerrainFeature(TerrainFeature terrainFeature) {
+        for (TerrainType terrainType : TerrainType.values())
+            if (terrainType.terrainFeature.equals(terrainFeature)) return terrainType;
+        return null;
     }
 }
