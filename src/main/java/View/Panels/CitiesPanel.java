910f4d4189a397580c4a1a435cc57dbb7436b4d2
parsa <mpmb81@gmail.com>
2022-05-09 16:26:36 +0430

    city panel, panel print

--- src/main/java/View/Panels/CitiesPanel.java
+++ src/main/java/View/Panels/CitiesPanel.java
@@ -1,14 +1,38 @@
 package View.Panels;
 
+import Controller.GameController;
+import Model.City;
+import Model.Player;
 import View.GameMenu;
+import enums.Color;
+
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class CitiesPanel extends GameMenu {
+
+    private static final ArrayList<City> cities = new ArrayList<>();
+
+    {
+        for (Player player : GameController.getGame().getPlayers()) {
+            for (City city : player.getCities()) {
+                cities.add(city);
+            }
+        }
+        cities.sort(Comparator.comparing(City::getName));
+    }
+
+
     public static void run(String command) {
 
 
     }
 
     private static void printPanel() {
-
+        int i = 0;
+        for (City city : cities) {
+            i++;
+            System.out.println(city.getOwner().getBackgroundColor().code + i + "- " + city.getName() + " \t" + city.getOwner().getName() + Color.RESET);
+        }
     }
 }
