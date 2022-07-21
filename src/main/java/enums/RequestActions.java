ca34537222abb5263e210687c3a7b9bcc46a008d
parsa <mpmb81@gmail.com>
2022-07-21 13:53:16 +0430

    inGameCommandsAreNowHandled in InGameCommandHandler.java

--- src/main/java/enums/RequestActions.java
+++ src/main/java/enums/RequestActions.java
@@ -7,7 +7,14 @@ public enum RequestActions {
     CHANGE_PROFILE_PICTURE("change_profile_pic"),
     GET_THIS_USER("get_this_user"),
     CHANGE_NICKNAME("change_nickname"),
-    CHANGE_PASSWORD("change_password"), GET_GAME_MAP("get_game_map"), GET_INVITATIONS("get_invitations"), SEND_INVITATIONS("send_invitaions"), ARE_INVITATIONS_ACCEPTED("are_reqs_accepted"), ACCEPT_INVITAION("Accept_request"), GET_USER_BY_USERNAME("get_user_by_username");
+    CHANGE_PASSWORD("change_password"),
+    GET_GAME_MAP("get_game_map"),
+    GET_INVITATIONS("get_invitations"),
+    SEND_INVITATIONS("send_invitaions"),
+    ARE_INVITATIONS_ACCEPTED("are_reqs_accepted"),
+    ACCEPT_INVITAION("Accept_request"),
+    GET_USER_BY_USERNAME("get_user_by_username"),
+    PANEL_COMMAND("panel_command");
 
     public final String code;
 
